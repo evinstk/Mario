697bac3e25559e568e51670de7bac4a19dca0c66
Tanner Evins <tanner@tannerevins.com>
2017-02-12 09:23:52 -0600

    Split ID types into separate header

--- src/world_state.hpp
+++ src/world_state.hpp
@@ -1,36 +1,11 @@
 #pragma once
+#include "id.hpp"
 #include <EASTL/vector_map.h>
 #include <EASTL/vector.h>
 #include <glm/glm.hpp>
 #include <GL/glew.h>
 
 namespace te {
-
-enum class id_type_t {
-	ENTITY,
-	ANIMATION,
-	ANIMATION_CONTROLLER
-};
-
-template <id_type_t I, typename T = int>
-struct id_t {
-	T id;
-	id_t() : id( 0 ) {}
-	explicit id_t( T _id ) : id( _id ) {}
-	bool operator<( const id_t& rhs ) const noexcept {
-		return id < rhs.id;
-	}
-	bool operator==( const id_t& rhs ) const noexcept {
-		return id == rhs.id;
-	}
-	bool operator!=( const id_t& rhs ) const noexcept {
-		return !( id == rhs.id );
-	}
-};
-
-using entity_t = id_t<id_type_t::ENTITY>;
-using animid_t = id_t<id_type_t::ANIMATION>;
-using animctrlid_t = id_t<id_type_t::ANIMATION_CONTROLLER>;
 
 struct map_t {
 	glm::ivec2 size;
