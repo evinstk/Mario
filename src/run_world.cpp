fd36f62f8e89ac8a1008cdc561b009e6bad35675
Tanner Evins <tanner@tannerevins.com>
2017-02-13 15:23:54 -0600

    Run view

--- src/run_world.cpp
+++ src/run_world.cpp
@@ -1,5 +1,7 @@
 #include "reducer_world.hpp"
 #include "game_state.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 #include <cassert>
 
 namespace te {
@@ -8,6 +10,13 @@ static void runPlayerEntity(entity_t& playerEntity, levelid_t levelID, const lev
 	auto playerEntityIt = playerObjectState.find(levelID);
 	assert(playerEntityIt != playerObjectState.end());
 	playerEntity = entity_t(playerEntityIt->second);
+}
+
+static void runView(glm::mat4& view,
+					const glm::vec3& playerTranslation) {
+	view = glm::translate(glm::ivec3(-playerTranslation.x,
+									 -playerTranslation.y,
+									 0));
 }
 
 void runWorld(worldstate_t& state, levelid_t levelID, const levelstate_t& levelState) {
@@ -21,6 +30,7 @@ void runWorld(worldstate_t& state, levelid_t levelID, const levelstate_t& levelS
 
 	runEntity(state.entity, levelID, levelState);
 	runPlayerEntity(state.playerEntity, levelID, levelState.playerObject);
+	runView(state.view, state.entity.translations[state.playerEntity]);
 }
 
 }
