1af17ec6a95f42537b9750c4fc45d724d3560dad
Tanner Evins <tanner@tannerevins.com>
2017-02-14 13:05:06 -0600

    Only jump if grounded

--- src/input_world.cpp
+++ src/input_world.cpp
@@ -1,5 +1,6 @@
 #include "reducer_world.hpp"
 #include "world_state.hpp"
+#include "game_state.hpp"
 #include <SDL.h>
 
 namespace te {
@@ -8,7 +9,9 @@ void inputWorld(worldstate_t& state, const SDL_Event& evt, const gamestate_t& ga
 	static constexpr float JUMP_SPEED = -6.5f * 32;
 
 	if (evt.type == SDL_KEYDOWN) {
-		if (evt.key.keysym.sym == SDLK_SPACE) {
+		if (evt.key.keysym.sym == SDLK_SPACE &&
+			game.world.entity.grounded.find(state.playerEntity) != game.world.entity.grounded.end()) {
+
 			state.entity.velocities[state.playerEntity].y = JUMP_SPEED;
 		}
 	}
