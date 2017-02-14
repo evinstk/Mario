d56d836c0c19192bdc9de8951b9a975082613a8b
Tanner Evins <tanner@tannerevins.com>
2017-02-14 12:23:37 -0600

    Mark pipes as solid

--- assets/tilesets/platform.tsx
+++ assets/tilesets/platform.tsx
@@ -3,7 +3,47 @@
  <image source="../images/mario-tileset.png" width="528" height="448"/>
  <tile id="0">
   <properties>
-   <property name="solid" value="true"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="264">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="265">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="297">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="298">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="330">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="331">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="363">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="364">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
 </tileset>
