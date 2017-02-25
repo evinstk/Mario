e390fe4f8963c7c55de1dd63e05bad3e02390c27
Tanner Evins <tanner@tannerevins.com>
2017-02-25 10:06:38 -0600

    Implement TextRenderer

--- src/text_renderer.cpp
+++ src/text_renderer.cpp
@@ -1,16 +1,19 @@
 #include "text_renderer.hpp"
 #include "game_values.hpp"
+#include <tegl/util.hpp>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include <GL/glew.h>
+#include <glm/gtc/type_ptr.hpp>
 #include <EASTL/vector.h>
+#include <EASTL/algorithm.h>
+#include <EASTL/iterator.h>
 #include <cassert>
 
 namespace te {
 
-constexpr int numChars = 128;
-
 TextRenderer::TextRenderer()
+	: m_bufSize(0)
 {
 	FT_Library ft;
 	FT_Error err = FT_Init_FreeType(&ft);
@@ -38,13 +41,20 @@ TextRenderer::TextRenderer()
 		}
 	}
 
-	const int cellSize = maxWidth * maxHeight;
+	m_cellSize.x = maxWidth;
+	m_cellSize.y = maxHeight;
+
+	int cellSize = maxWidth * maxHeight;
 	const int size = cellSize * numChars;
 	eastl::vector<GLubyte> pixels(size, 0);
 	for (GLubyte ch = 0; ch < numChars; ++ch) {
 
 		FT_Error err = FT_Load_Char(face, ch, FT_LOAD_RENDER);
 		assert(err == 0);
+
+		m_bearings[ch].x = face->glyph->bitmap_left;
+		m_bearings[ch].y = face->glyph->bitmap_top;
+		m_advances[ch]   = face->glyph->advance.x >> 6;
 
 		const unsigned char *pBuffer = face->glyph->bitmap.buffer;
 
@@ -57,7 +67,8 @@ TextRenderer::TextRenderer()
 		}
 	}
 
-	glBindTexture(GL_TEXTURE_2D, m_CharTexture);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glBindTexture(GL_TEXTURE_2D, m_charTexture);
 	glTexImage2D(GL_TEXTURE_2D,
 				 0,
 				 GL_RED,
@@ -75,6 +86,101 @@ TextRenderer::TextRenderer()
 
 	FT_Done_Face(face);
 	FT_Done_FreeType(ft);
+
+	glBindVertexArray( m_vertexArray );
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_translationBuf);
+	glVertexAttribIPointer(0, 2, GL_INT, 0, 0);
+	glVertexAttribDivisor(0, 1);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_textBuf);
+	glVertexAttribIPointer(1, 1, GL_INT, 0, 0);
+	glVertexAttribDivisor(1, 1);
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	Shader vertexShader( GL_VERTEX_SHADER );
+	auto vertexShaderSrc = LoadFile( "shaders/text.glvs" );
+	CompileShader( vertexShader, vertexShaderSrc.c_str() );
+
+	Shader fragmentShader( GL_FRAGMENT_SHADER );
+	auto fragmentShaderSrc = LoadFile( "shaders/text.glfs" );
+	CompileShader( fragmentShader, fragmentShaderSrc.c_str() );
+
+	glAttachShader( m_shader, vertexShader );
+	glAttachShader( m_shader, fragmentShader );
+	glLinkProgram( m_shader );
+
+	GLint programSuccess = GL_TRUE;
+	glGetProgramiv( m_shader, GL_LINK_STATUS, &programSuccess );
+	assert( programSuccess == GL_TRUE );
+
+	m_projectionLoc = glGetUniformLocation(m_shader, "projection");
+	m_cellSizeLoc   = glGetUniformLocation(m_shader, "cellSize");
+
+	assert(glGetError() == GL_NO_ERROR);
+}
+
+void TextRenderer::draw(const eastl::string& text,
+						const glm::ivec2& location,
+						const glm::mat4& projection) {
+	glUseProgram(m_shader);
+
+	glUniformMatrix4fv(m_projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	glUniform2iv(m_cellSizeLoc, 1, glm::value_ptr(m_cellSize));
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_charTexture);
+
+	using translation_t = glm::ivec2;
+	using text_t = int;
+
+	auto textSize = text.size();
+	if (textSize > m_bufSize) {
+		glBindBuffer(GL_ARRAY_BUFFER, m_translationBuf);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(translation_t) * textSize, NULL, GL_DYNAMIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, m_textBuf);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(text_t) * textSize, NULL, GL_DYNAMIC_DRAW);
+		m_bufSize = textSize;
+	}
+
+	eastl::vector<text_t> intText;
+	intText.reserve(textSize);
+	eastl::transform(text.begin(), text.end(), eastl::back_inserter(intText), [](auto ch) {
+			return (text_t)ch;
+		});
+	glBindBuffer(GL_ARRAY_BUFFER, m_textBuf);
+	glBufferSubData(GL_ARRAY_BUFFER,
+					0,
+					sizeof(text_t) * textSize,
+					intText.data());
+
+	eastl::vector<translation_t> translations;
+	translations.reserve(textSize);
+	int baseX = location.x;
+	eastl::transform(text.begin(), text.end(), eastl::back_inserter(translations), [baseX, this, &location] (auto ch) mutable {
+			glm::ivec2 translation(baseX + m_bearings[ch].x, location.y - m_bearings[ch].y);
+			baseX += m_advances[ch];
+			return translation;
+		});
+	glBindBuffer(GL_ARRAY_BUFFER, m_translationBuf);
+	glBufferSubData(GL_ARRAY_BUFFER,
+					0,
+					sizeof(translation_t) * textSize,
+					translations.data());
+
+	glBindVertexArray(m_vertexArray);
+	glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, textSize);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUseProgram(0);
+
+	assert(glGetError() == GL_NO_ERROR);
 }
 
 } // namespace te
--- src/text_renderer.hpp
+++ src/text_renderer.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <tegl/types.hpp>
+#include <glm/glm.hpp>
+#include <EASTL/array.h>
+#include <EASTL/string.h>
 
 namespace te {
 
@@ -8,9 +11,27 @@ struct gamestate_t;
 class TextRenderer {
 public:
 	TextRenderer();
-	void draw(const gamestate_t& state);
+	void draw(const eastl::string& text,
+			  const glm::ivec2& location,
+			  const glm::mat4& projection);
 private:
-	Texture m_CharTexture;
+	static constexpr int numChars = 128;
+
+	eastl::array<glm::ivec2, numChars> m_bearings;
+	eastl::array<int, numChars> m_advances;
+	glm::ivec2 m_cellSize;
+	Texture m_charTexture;
+
+	VertexArray m_vertexArray;
+
+	Buffer m_translationBuf;
+	Buffer m_textBuf;
+	int m_bufSize;
+
+	ShaderProgram m_shader;
+
+	GLuint m_projectionLoc;
+	GLuint m_cellSizeLoc;
 };
 
 } // namespace te
